b27081046c239c27989a4ce8f32f1658a22ba012
Holvert Josué Pérez Talavera <50637750+Holvert-P@users.noreply.github.com>
2021-11-19 15:35:55 -0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <!-- Script -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD PHP MySQL</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/b69d3fa8e3.js" crossorigin="anonymous"></script>
+   
+</head>
+<body>
+<nav class="navbar navbar-dark bg-dark">
+<div class="container">
+<a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+</div> 
+</nav>
